e8c083246cdf87e4d7125df5713495c10c74f3a6
Dan <dnlsfq@gmail.com>
2020-09-05 15:55:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// var myString = "Test";
+// function myFunction(){
+// 	console.log(myString);
+// }
+
+// myFunction();
+
+
+// line break
+// var good = '<ul id="myId">' +
+// 		   '<li> writing text </li>' +
+// 		   '<li> writing text </li>' +
+// 		   '</ul>';
+
